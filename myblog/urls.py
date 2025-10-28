b0b784bb4d53b2b1481ad9969329cdbb81ac4978
AL Shahriar Ahamed Rafsun <rafsunupwork@gmail.com>
2025-10-28 11:41:19 +0600

    Updated

--- myblog/urls.py
+++ myblog/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from blog import views
 urlpatterns = [
-    path('blog/', include('blog.urls')),
+    path('', views.home, name='home'),
+    path('', include('blog.urls')),
     path('admin/', admin.site.urls),
 ]
